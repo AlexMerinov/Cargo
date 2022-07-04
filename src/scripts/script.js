4a323adecb7afea85af5774059f0e84239c1a1ff
John Doe <alexmerinov46@gmail.com>
2022-07-04 17:22:17 +0300

    add card

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,15 +5,14 @@ openHeaders.forEach((openHeader) => {
    openHeader.addEventListener('click', listOpen);
 });
 
-function listOpen(e) {
-   e.preventDefault();
-   let currentItem = e.target.closest('.footer-col__header');
-   let currentList = e.target.nextElementSibling;
-   currentItem.classList.toggle('footer-col__header--active');
-   if (currentItem.classList.contains('footer-col__header--active')) {
-      currentList.style.maxHeight = currentList.scrollHeight + "px";
-   } else {
-      currentList.style.maxHeight = 0;
+   function listOpen(e) {
+      let currentItem = e.target.closest('.footer-col__header');
+      let currentList = e.target.nextElementSibling;
+      currentItem.classList.toggle('footer-col__header--active');
+      if (currentItem.classList.contains('footer-col__header--active')) {
+         currentList.style.maxHeight = currentList.scrollHeight + "px";
+      } else {
+         currentList.style.maxHeight = 0;
+      }
    }
-}
 }
