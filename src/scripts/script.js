c84a2a46c40c32a6ba320107d2eca7de153b98af
John Doe <alexmerinov46@gmail.com>
2022-07-13 17:20:44 +0300

    noUiSlider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -16,3 +16,48 @@ openHeaders.forEach((openHeader) => {
       }
    }
 }
+
+
+const ShowMoreButtons = document.querySelectorAll('.text__link');
+
+if (ShowMoreButtons) {
+   ShowMoreButtons.forEach((ShowMoreButton) => {
+      ShowMoreButton.addEventListener("click", function () {
+         let textVisibles = document.querySelectorAll(".text__invisible");
+         textVisibles.forEach(function (textVisible) {
+            textVisible.classList.toggle('text__invisible--active');
+            if (textVisible.classList.contains('text__invisible--active')) {
+               textVisible.style.maxHeight = textVisible.scrollHeight + "px";
+            } else {
+               textVisible.style.maxHeight = 0;
+            }
+         })
+      })
+   });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+   const slider = document.getElementById('slider');
+   const minMaxValue = document.querySelectorAll('.range-slider__value');
+
+
+   noUiSlider.create(slider, {
+      start: [80, 8000000],
+      connect: true,
+      step: 1,
+      range: {
+         'min': 80,
+         'max': 8000000
+      },
+   });
+
+   slider.noUiSlider.on('change', (values) => {
+      let minValue = values[0];
+      let maxValue = values[1];
+
+      minMaxValue[0].value = minValue;
+      minMaxValue[1].value = maxValue;
+   })
+
+});
+
