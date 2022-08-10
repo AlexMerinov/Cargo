86a805a9bb7e4698c8c810cef072246e5d6d27ed
Alex Merinov <alexmerinov46@gmail.com>
2022-08-10 17:34:11 +0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -30,142 +30,99 @@ document.addEventListener("DOMContentLoaded", () => {
       }
    }
 
-   
-// SEO-text dropbox -------------------------------------->
-
-   const ShowMoreButtons = document.querySelectorAll('.text__link');
-   const textHide = document.querySelector('.js-text-hide');
-   
-   if (ShowMoreButtons) {
-      ShowMoreButtons.forEach((ShowMoreButton) => {
-         ShowMoreButton.addEventListener("click", function () {
-            let textVisibles = document.querySelectorAll(".text__invisible");
-            textVisibles.forEach(function (textVisible) {
-               textVisible.classList.toggle('text__invisible--active');
-               if (textVisible.classList.contains('text__invisible--active')) {
-                  textVisible.style.maxHeight = textVisible.scrollHeight + "px";
-                  ShowMoreButton.innerHTML = 'Свернуть';
-                  textHide.classList.add('js-text-hide--no-icon');
-               } else {
-                  textVisible.style.maxHeight = 0;
-                  ShowMoreButton.innerHTML = 'Развернуть';
-                  textHide.classList.remove('js-text-hide--no-icon');
-               }
-            })
-         })
-      });
-   }
 
 // RangeSlider -------------------------------------->
 
-   const rangeSlider = document.getElementById('range-slider');
+   // const rangeSlider = document.getElementById('range-slider');
 
-   if (rangeSlider) {
+   // if (rangeSlider) {
 
-      noUiSlider.create(rangeSlider, {
-         start: [80, 8000000],
-         connect: true,
-         step: 1,
-         range: {
-            'min': [80],
-            'max': [8000000]
-         },
-      });
+   //    noUiSlider.create(rangeSlider, {
+   //       start: [80, 8000000],
+   //       connect: true,
+   //       step: 1,
+   //       range: {
+   //          'min': [80],
+   //          'max': [8000000]
+   //       },
+   //    });
 
-      const input0 = document.getElementById('input-0');
-      const input1 = document.getElementById('input-1');
-      const inputs = [input0, input1];
+   //    const input0 = document.getElementById('input-0');
+   //    const input1 = document.getElementById('input-1');
+   //    const inputs = [input0, input1];
 
-      rangeSlider.noUiSlider.on('update', function (values, handle) {
-         inputs[handle].value = (values[handle]);
-      });
+   //    rangeSlider.noUiSlider.on('update', function (values, handle) {
+   //       inputs[handle].value = (values[handle]);
+   //    });
 
-      const setRangeSlider = (i, value) => {
-         let arr = [null, null];
-         arr[i] = value;
+   //    const setRangeSlider = (i, value) => {
+   //       let arr = [null, null];
+   //       arr[i] = value;
 
-         rangeSlider.noUiSlider.set(arr);
-      };
+   //       rangeSlider.noUiSlider.set(arr);
+   //    };
 
-      inputs.forEach((el, index) => {
-         el.addEventListener('change', (e) => {
-            setRangeSlider(index, e.currentTarget.value)
-         });
-      });
-   }
+   //    inputs.forEach((el, index) => {
+   //       el.addEventListener('change', (e) => {
+   //          setRangeSlider(index, e.currentTarget.value)
+   //       });
+   //    });
+   // }
 
 // anchors choise item in header list
-
-   const choiceAnchors = document.querySelectorAll('.choice__input');
-
-   for (let i = 0, len = choiceAnchors.length; i < len; i++) {
-      let thisAnchor = choiceAnchors[i];
-      let nameTitle = document.querySelector('.drop-down__name').textContent;
-      thisAnchor.onclick = newTitle;
-      let checkedArray = [];
-
-      function newTitle() {
-         if (thisAnchor.checked) {
-            let activeAnchor = this.closest('.choice').querySelector('.choice__name').textContent;
-            let dropDownName = this.closest('.drop-down__item').querySelector('.drop-down__name');
-            checkedArray.push(activeAnchor);
-            dropDownName.innerHTML = checkedArray;
-            console.log(checkedArray);
-         } else {
-            let activeAnchor = this.closest('.choice').querySelector('.choice__name').textContent;
-            let dropDownName = this.closest('.drop-down__item').querySelector('.drop-down__name');
-            checkedArray.splice(activeAnchor);
-            dropDownName.innerHTML = nameTitle;
-         };
-      };
-   }
-
-/*custom input number-------------------------------->*/
-   const quantityValue = document.querySelector('.input-value');
-   const valueBtnsMunus = document.querySelector('.btn-value--prev');
-   const valueBtnsPlus = document.querySelector('.btn-value--next');
-
-   if (quantityValue) {
-      valueBtnsMunus.addEventListener('click', function () {
-         if (quantityValue.value > 0) {
-            quantityValue.value = quantityValue.value - 1;
-         }
-      });
-      valueBtnsPlus.addEventListener('click', function () {
-         quantityValue.value = +quantityValue.value + 1;
-      });
-   }
 
 
    /*-----------------Slider categories-------------------------------*/
 
-   const itemCaruselBig = new Swiper('.carusel-big__slider', { 
+   const promoSlider = new Swiper('.promo-carusel__slider', { 
 
       slidesPerView: 1,
-      spaceBetween: 10,
+      spaceBetween: 5,
 
-      thumbs: {
-         swiper: {
-            el: '.carusel-mini__slider',
-            slidesPerView: 1,
-            spaceBetween: 10,
-            breakpoints: {
-               768: {
-                  slidesPerView: 3,
-               }
-            },
+      navigation: {
+         nextEl: '.promo-carusel__btn--next',
+         prevEl: '.promo-carusel__btn--prev',
+      },
 
-            navigation: {
-               nextEl: '.carusel-mini__btn--next',
-               prevEl: '.carusel-mini__btn--prev',
-            },
-         }
-      }
+      // breakpoints: {
+      //    768: {
+      //       navigation: false,
+      //    }
+      // },
 
    });
 
-   /*-----------------light box-------------------------------*/
+   const advantagesTabs = document.querySelectorAll('.advantages-box');
+   if (advantagesTabs) {
 
+      advantagesTabs.forEach((advantagesTab) => {
+
+         const tabsBtn   = advantagesTab.querySelectorAll(".advantages-pagination__btn");
+         const tabsItems = advantagesTab.querySelectorAll('.advantages-content__item');
+      
+         tabsBtn.forEach(function(item) {
+            item.addEventListener("click", function() {
+
+               let currentBtn = item;
+               let tabId      = currentBtn.getAttribute("data-tab");
+               let currentTab = document.querySelector(tabId);
+      
+               tabsBtn.forEach(function(item) {
+                  item.classList.remove('advantages-pagination__btn--active');
+               });
+      
+               tabsItems.forEach(function(item) {
+                  item.classList.remove('advantages-content__item--active');
+               });
+      
+               currentBtn.classList.add('advantages-pagination__btn--active');
+               currentTab.classList.add('advantages-content__item--active');
+               
+            });
+         });
+
+      });
    
+   }
 });
 
